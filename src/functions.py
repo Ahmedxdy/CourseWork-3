5b186bfc456bc476b4da7dee36757cc5d26a6efe
Surik4tik <romans310795@gmail.com>
2024-06-03 20:58:31 +0300

    Функция вывода даты в требуемом формате

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 
 def load_json(file_name, encoding='utf-8'):
@@ -6,3 +7,16 @@ def load_json(file_name, encoding='utf-8'):
     with open(file_name, "r", encoding="utf-8") as f:
         data = json.load(f)
     return data
+
+
+def true_date(dict_):
+    """Обработка строки с датой"""
+    date = dict_["date"]
+    list_ = re.split("[T-]", date)
+    result = ".".join(list_[2::-1])
+    return result
+
+
+# def reversed_executed_data(data):
+#     """Обработка данных клиента в обратном порядке и получение словаря с успешными операциями"""
+#     pass
