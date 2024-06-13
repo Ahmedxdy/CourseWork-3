a741ad801d58c05e5f8b830819cbf2f2f33d15ac
Surik4tik <romans310795@gmail.com>
2024-06-13 20:31:14 +0300

    Реализован алгоритм вывода 5 последних операций пользователя

--- src/main.py
+++ src/main.py
@@ -4,5 +4,13 @@ file_name = "operations.json"
 
 if __name__ == '__main__':
     data = load_json(file_name)
-    for i in data:
-        print(i["id"])
+    count = 0
+    for dict_ in reversed(data):
+        if count == 5:
+            break
+        if dict_.get("state") == None:
+            continue
+        if dict_["state"] == "EXECUTED":
+            print(true_date(dict_), card_and_bank_account(dict_), money(dict_), sep="\n", end="\n\n")
+            count += 1
+        
